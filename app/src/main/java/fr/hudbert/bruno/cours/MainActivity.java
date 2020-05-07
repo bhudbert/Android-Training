3b39ce5096a924a42d6f4eb49c872b2b6c1c5343
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-05-07 16:45:29 +0200

    new start with Oreo

--- app/src/main/java/fr/hudbert/bruno/cours/MainActivity.java
+++ app/src/main/java/fr/hudbert/bruno/cours/MainActivity.java
@@ -1,14 +1,15 @@
-package fr.hudbert.bruno.module1;
+package fr.hudbert.bruno.cours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 }
